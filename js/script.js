f740039a0b0faae2217424115c0a0dbc2cb92f17
Jessica <jesss8692@hotmail.com>
2024-01-05 17:16:03 +0100

    Update drawCard function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const cardsContainer =  document.querySelector('.cards-container');
 const gameStateMsg = document.querySelector('.game-state-msg');
 const playBtn = document.querySelector('.play-btn');
+const newCardBtn = document.querySelector('.new-card-btn');
 const rulesBtn = document.querySelector('.rules-btn');
 const rulesSection = document.querySelector('.rules');
 const cardDeck = [
-    'C1.png','C2.png','C3.png','C4.png','C5.png','C6.png','C7.png','C8.png','C9.png',
+    'C1s.png','C2.png','C3.png','C4.png','C5.png','C6.png','C7.png','C8.png','C9.png',
     'C10.png','C11.png','C12.png','C13.png',
     'h1.png','h2.png','h3.png','h4.png','h5.png','h6.png','h7.png','h8.png','h9.png',
     'h10.png','h11.png','h12.png','h13.png',
@@ -16,29 +17,40 @@ const cardDeck = [
     't10.png','t11.png','t12.png','t13.png'
 ];
 let playerCards = [];
+let sum = 0;
 let isPlaying = false;
+let hasBlackJack = false;
+let message = "";
 
-playBtn.addEventListener('click', playGame);
+
+playBtn.addEventListener('click', startGame);
+newCardBtn.addEventListener('click', drawCard);
 rulesBtn.addEventListener('click', showRules);
+
+function startGame() {
+    playerCards = [cardDeck[getRandomIndex()], cardDeck[getRandomIndex()]];
+    isPlaying = true;
+    for (let i = 0; i < playerCards.length; i++) {
+        createCardElement(playerCards[i]);
+    }
+}
+
+
+function getCardsValue() {
+    // loop through cars array and extract number value of card (max first two indexes)
+    // if card array index 1 is a number concatenate index 0 and 1 and convert it to number 
+    // if card value is greater than 10 set it equal to 10
+    // if card value is 1 set it equal to 11
+    // add the cards values to sum
+    for (let i = 0; i < cardDeck.length; i++) {
+
+    }
+}
 
 function drawCard() {
     playerCards.push(cardDeck[getRandomIndex()]);
-    playGame();
-}
-
-function playGame() {
-    if (!isPlaying) {
-        playerCards = [cardDeck[getRandomIndex()], cardDeck[getRandomIndex()]];
-        isPlaying = true;
-    } 
-    for (let i = 0; i < playerCards.length; i++) {
-        let card = document.createElement('img');
-        card.classList.add('card-img');
-        card.setAttribute('src', `./img/${playerCards[i]}`);
-        card.setAttribute('alt', 'Image of playing card')
-        cardsContainer.appendChild(card);
-        console.log(card)
-    }
+    createCardElement(playerCards[playerCards.length -1]);
+    
 }
 
 function showRules() {
@@ -48,4 +60,12 @@ function showRules() {
 
 function getRandomIndex() {
     return Math.floor(Math.random() * cardDeck.length)
+}
+
+function createCardElement(img) {
+    let card = document.createElement('img');
+    card.classList.add('card-img');
+    card.setAttribute('src', `./img/${img}`);
+    card.setAttribute('alt', 'Image of playing card')
+    cardsContainer.appendChild(card);
 }
